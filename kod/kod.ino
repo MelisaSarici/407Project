70b19769a0dfb15553e9dc6c7b425ba1d50e3b6f
MelisaSarici <sarici.asena@gmail.com>
2018-01-24 16:02:01 +0200

    vovooo

--- kod/kod.ino
+++ kod/kod.ino
@@ -4,8 +4,8 @@
 #define PWM1          6                     // PWM motor pin ,Be careful in case of change to put the PWM to a pin able to generate PWM
 #define encoder0PinA  2                      // DO NOT CHANGE THIS, FOR INTERRUPT NECESSARY!!
 
-#define TIMERSTEP       400            // Defined as 1 sec (Arduino counts the time in microsecs) in order to set the frequency in a more user friendly way
- TimerObject *timer1 = new TimerObject(4000);                        // If you change the TIMERSTEP, change the velocity equation as well accordingly
+#define TIMERSTEP       40000            // Defined as 1 sec (Arduino counts the time in microsecs) in order to set the frequency in a more user friendly way
+ TimerObject *timer1 = new TimerObject(400);                        // If you change the TIMERSTEP, change the velocity equation as well accordingly
 
 //volatile unsigned long int :if you dont get a desired value or accuracy for encoder readings change float to this
 
@@ -14,7 +14,7 @@ int timercounterA=0;  // counts the rising edges of the encoder reading
 float velocityA=0;
 float position1A=0;  // stores the previous position in order to calculate the velocity
 float position2A=0;  // latest position
-int D=0;
+static int D=0;
 
 void setup() {
   
@@ -26,14 +26,15 @@ void setup() {
 
   Serial.begin(115200);
   setPwmFrequency(6, 8); //Sets the pwm output of 5 to around 8 to 9 kHz
-    
+  
+    timer1->setOnTimer(&duty);
+    timer1->Start();
+       
   Timer1.attachInterrupt(callback);       //start interrupt on main using initialized Timer, helps to mask the variables also, if used somewhere else, the operation is not affected 
   // encoder pin on interrupt 0 (pin 2)
   attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoderA, RISING); // at each Timer1 period go to interrupt execute doEncoderA
   // encoder pin on interrupt 1 (pin 3)
- 
-    timer1->setOnTimer(&duty);
-    timer1->Start();
+
 
 
 }
@@ -83,7 +84,7 @@ void callback(){
 
  void duty(){
 
-      if D<100{
+      if (D<100){
         D++;
         }
         else if ((D>=100)&&(D<=110)){
@@ -91,22 +92,15 @@ void callback(){
 
         }
         else D=160;
+Serial.print("**********************");
 
-      
-      Serial.print("Distance in CM: ");
-       // Pass INC as a parameter to get the distance in inches
-      Serial.println(a);
   }
   
 void loop() {
 // put your main code here, to run repeatedly:
+       timer1->Update();
+      analogWrite(PWM1, D);
       
-      analogWrite(PWM1, 0);
-  
-else {
-    analogWrite(PWM1, 0);
-    analogWrite(PWM2, 0);
-    }
 }
 
 
